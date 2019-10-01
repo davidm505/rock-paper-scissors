693603dcc8809c68006bafcf78d5b14d51f6f181
davidm505 <damorales310@gmail.com>
2019-10-01 16:32:23 -0600

    Add console game.

---
+++ static/js/logic.js
@@ -0,0 +1,52 @@
+
+
+let getRndInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+
+let playerPlay = () => {
+    
+    choice = prompt("Enter Rock, Paper or Scissors!");
+
+    return choice.toLowerCase();
+    
+}
+
+
+function computerPlay() {
+    
+    let choices = ['rock', 'paper', 'scissors'];
+    
+    let number = getRndInt(0,3);
+
+    return choices[number];
+}
+
+
+function playRound(player, computer) {
+
+    if (player == 'rock') {
+        if (computer == 'rock') return 'Rock & Rock: Tie!'
+        else if (computer == 'paper') return 'Rock & Paper: Computer wins!'
+        else return 'Rock & Scissors: Player wins!'
+    } else if (player == 'paper') {
+        if (computer == 'rock') return 'Paper & Rock: Player wins!'
+        else if (computer == 'paper') return 'Paper & Paper: Tie!'
+        else return 'Ppaer & Scissors: Computer wins!'
+    } else {
+        if (computer == 'rock') return 'Scissors & Rock: Computer wins!'
+        else if (computer == 'paper') return 'Scissors & Paper: Player wins!'
+        else return 'Scissors & Scissors: Tie!'
+    }
+}
+
+
+const playerSelection = playerPlay();
+const computerSelection = computerPlay();
+
+
+function game() {
+    
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+game()
